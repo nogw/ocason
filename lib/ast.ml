474b0c8afc0e044cf94eddf168aedaf5d61d4c43
nogw <gabrielnogueiraoliveira@gmail.com>
2022-02-05 21:37:51 -0300

    (ReasonML -> OCaml) ast

---
+++ lib/ast.ml
@@ -0,0 +1,10 @@
+type json = 
+  | JsonNull
+  | JsonBool   of bool 
+  | JsonNumber of int 
+  | JsonFloat  of float 
+  | JsonString of string 
+  | JsonArray  of json list
+  | JsonObject of string * json list
+  
+[@@deriving ((show { with_path = false }), eq)]
